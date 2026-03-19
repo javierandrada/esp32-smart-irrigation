7dcf1e1536c12495f067dad04693259cbb1745dc
javierandrada <132535385+javierandrada@users.noreply.github.com>
2026-03-19 09:25:15 -0300

    Update oled.h

--- include/oled.h
+++ include/oled.h
@@ -4,10 +4,9 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <U8g2lib.h>
-#include "config_oled.h"
 
-#define SCL_PIN I2C_SCL_PIN
-#define SDA_PIN I2C_SDA_PIN
+#define SCL_PIN 21
+#define SDA_PIN 22
 
 void oled88_init(void);
 void oled88_clear(void);
@@ -17,3 +16,4 @@ void oled88_printTit(const char * tittle);
 void oled88_printRiego(int humedad, bool modo_auto, bool bomba_on);
 
 #endif
+
